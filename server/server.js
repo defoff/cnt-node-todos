cec7b56df8c611929c3462c14bb7eae05b29490d
RolfKoch <rolf.koch@deutschebahn.com>
2018-04-26 00:27:37 +0200

    server refactoring

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', function (req, res) {
+    console.log(req.body);
+   var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then( function (doc) {
+        res.send(doc);
+    }, function (e) {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, function() {
+    console.log('Started on port 3000');
+});
