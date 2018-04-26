54759c41abf94bae98467c36f4be77716ffd6a70
defoff <rolf.cook@googlemail.com>
2018-04-26 23:29:21 +0200

    changed port

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = 80;
 
 app.use(bodyParser.json());
 
